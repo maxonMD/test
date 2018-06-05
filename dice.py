16380518b48547331cf9131bd9fd82459d978c1d
maxonMD <maxon405@gmail.com>
2018-06-05 16:29:23 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -1,15 +1,20 @@
 import random
 
-
-def end(den):
-    try:
-        inscanner = int(input("Place your bet. Enter 0 to quit: "))
-        if inscanner == 0:
-            print("You have exited goodbye")
-            exit()
-    except ValueError:
-        print("nothing")
-        end(den)
+def end(amount):
+    if amount <= 0:
+        print("You are out of money")
+        exit()
+    if amount > 0:
+        while True:
+            try:
+                play = int(input("Enter 1 to continue or 0 to quit"))
+                if play == 1:
+                    break
+                elif play == 0:
+                    exit()
+            except ValueError:
+                print("Please enter 1 to play or 0 to quit")
+                continue
 
 def determinewinnings(bet, c, total, amount):
     if c == 'h' and total >= 8:
@@ -19,8 +24,8 @@ def determinewinnings(bet, c, total, amount):
         print("You have won " + str(bet))
         return amount + bet
     elif c == 's' and total == 7:
-        print("You have won " + str(bet*4))
-        return amount + bet*4
+        print("You have won " + str(bet * 4))
+        return amount + bet * 4
     else:
         print("You have lost " + str(bet))
         return amount - bet
@@ -29,64 +34,55 @@ def getroll():
     rolla = random.randint(1, 6)
     return rolla
 
-def getbet(amount, bet):
-    try:
-        if bet < 0 or bet > amount:
-            print("Please enter a reasonable bet")
-        elif bet == 0:
-            print("goodbye")
-            exit(0)
-        elif bet > 0 or bet < amount:
-            return bet
-    except ValueError:
-        print("please enter a valid number")
+def getbet(amount):
+    while True:
+        try:
+            print("You now have $" + str(amount) + " dollars")
+            bet = int(input("Place your bet "))
+            if bet < 0 or bet > amount:
+                print("Insufficient funds: try something smaller ")
+                continue
+            elif bet == 0:
+                print("goodbye")
+                exit(0)
+            elif bet > 0 or bet <= amount:
+                return bet
+        except ValueError:
+            print("please enter a valid number")
 
-
-def gethighlow(c):
-    if c == "s" or c == "h" or c == "l":
-        return c
-    else:
-        print("You have entered in an invalid character")
+def gethighlow():
+    while True:
+        try:
+            c = input("Enter H for high, S for sevens and L for low: ").lower()
+            if c == "s" or c == "h" or c == "l":
+                if c == "h":
+                    print("You bet high")
+                    return c
+                elif c == "l":
+                    print("You bet low")
+                    return c
+                elif c == "s":
+                    print("You bet sevens")
+                return c
+            else:
+                print("Enter a valid option")
+        except ValueError:
+            print("You have entered in an invalid character ")
 
 def game():
     amount = 100
     while True:
-        bet = int(input("Place your bet "))
-        try:
-            getbet(amount, bet)
-            c = input("Enter H for high, S for sevens and L for low: ").lower()
-            if amount <= 0:
-                print("You are out of money please go get some more")
-                exit()
-            try:
-                print("You have " + str(amount) + " dollars")
-                getbet(amount, bet)
-                den = False
-                while not den:
-                    gethighlow(c)
-                    while getbet(amount, bet) != 0 and amount >= 0:
-                        roll1 = getroll()
-                        roll2 = getroll()
-                        total = (roll1 + roll2)
-                        print("Dice 1 = " + str(roll1))
-                        print("Dice 2 = " + str(roll2))
-                        print("Therefore you rolled a " + str(total))
-                        if gethighlow(c) == "h":
-                            print("You bet high")
-                        elif gethighlow(c) == "l":
-                            print("You bet low")
-                        elif gethighlow(c) == "s":
-                            print("You bet sevens")
-                        amount = determinewinnings(bet, c, total, amount)
-                        print("You now have $" + str(amount) + " dollars")
-                        #end(den)
-                        den = True
-                        break
-            except ValueError:
-                print("Please place a bet based on your remaining funds")
-                continue
-        except ValueError:
-            print("please enter a valid item")
-            continue
+            bet = getbet(amount)
+            c = gethighlow()
+            print("You have " + str(amount) + " dollars")
+            roll1 = getroll()
+            roll2 = getroll()
+            total = (roll1 + roll2)
+            print("Dice 1 = " + str(roll1))
+            print("Dice 2 = " + str(roll2))
+            print("The total of your roll is " + str(total))
+            amount = determinewinnings(bet, c, total, amount)
+            print("You now have " + str(amount) + " dollars")
+            end(amount)
 
 game()
